44e1d06f6fd00490e80feea2bee96ac60d1ad21e
sexy_wowoto <csjaj9772@gmail.com>
2016-12-04 21:53:12 +0900

    error edited

--- refiner.py
+++ refiner.py
@@ -13,44 +13,7 @@ def prepare(source):
     source = source.replace("\\dbinom", "\\binom")
     source = source.replace("\\mybinom", "\\binom")
     source = source.replace("  ", " ")
-    source = source.replace("{ {", "{{")
-    source = source.replace("} }", "}}")
     return source 
-
-def handle(source):
-    # erase useless parentheses
-
-    source = prepare(source)
-
-    length = len(source)
-    chk = [0] * length
-    stk = []
-    pairs = []
-    
-    for i in xrange(0, length):
-        if source[i] == '{':
-            stk.append(i)
-        elif source[i] == '}':
-            try:
-                l = stk.pop()
-                r = i
-                pairs.append([l,r])
-            except:
-                raise Exception
-
-    plength = len(pairs)
-
-    for i in xrange(0, plength-1):
-        if pairs[i][0]-1 == pairs[i+1][0]:
-            if pairs[i][1]+1 == pairs[i+1][1]:
-                chk[pairs[i][0]] = 1
-                chk[pairs[i][1]] = 1
-
-    data = ""
-    for i in xrange(0, length):
-        if chk[i] == 0:
-            data += source[i]
-    return data
 
 def convertLtxToMathml(ltx):
     
