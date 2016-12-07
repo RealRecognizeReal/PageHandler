e258a6a7f85432515e025886e49a5e3916707423
sexy_wowoto <csjaj9772@gmail.com>
2016-12-08 07:48:55 +0900

    final

--- sets/data/faster.py
+++ sets/data/faster.py
@@ -5,16 +5,38 @@ from heapq import heappush, heappop
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
-lst = []
-
 CORE = 16
 
-with open("./merge00.out") as infile:
+prev = ""
+
+ltx_mapper = {}
+
+# key: latex, value: array of url
+
+# key: x^2 url: ab, bc, de
+
+# dict["x^2"] = ["ab"]
+# dict["x^2"].append("bc")
+with open("./merge.out") as infile:
     for line in infile:
         line = line[:-1]
-        lst.append(line)
 
-SIZE = len(lst)
+        if len(line) <= 0:
+            prev = line
+            continue
+
+        if line[0] == '2':
+            ltx = prev
+            url = line[2:]
+            
+            if ltx not in ltx_mapper.keys():
+                ltx_mapper[ltx] = []
+            
+            ltx_mapper[ltx].append(url)
+            
+        prev = line[2:]
+
+SIZE = len(ltx_mapper.keys())
 
 ass = [0] * CORE
 rem = SIZE % CORE
@@ -48,14 +70,14 @@ class mWorker (threading.Thread):
 
         self.doWork()
 
-goal = raw_input()
+goal = sys.argv[1]
 pq = []
 mworker = [mWorker] * CORE
 st = 0
 
 for i in range(0, CORE):
     ed = st+ass[i]
-    mworker[i] = mWorker(st, ed, lst, pq, goal)
+    mworker[i] = mWorker(st, ed, ltx_mapper.keys(), pq, goal)
     st = ed
     mworker[i].start()
     
@@ -63,10 +85,30 @@ for i in range(0, CORE):
     if mworker[i].isAlive():
         mworker[i].join()
 
+temp = []
+
 for i in range(0, 100):
     if len(pq) <= 10:
-        print(heappop(pq))
+        (e, ltx) = heappop(pq)
+        temp.append(ltx_mapper[ltx])
         if len(pq) == 0:
             break
     else:
         heappop(pq)
+
+temp.reverse()
+
+answer = []
+
+for urls in temp:
+    for url in urls:
+        if url in answer:
+            continue
+        else:
+            answer.append(url)
+            if len(answer) == 10:
+                break
+    if len(answer) == 10:
+        break
+
+print(answer)
